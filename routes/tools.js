f408e9314a884f3e02b1771ce49cca586d1fd38c
doccenter <nguyentanhau165997@gmail.com>
2019-01-24 02:55:18 +0700

    hehe

--- routes/tools.js
+++ routes/tools.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const request = require('xhr-request');
 
+router.get('/tangtraffic', function (req, res) {
+    res.render('addtraffic', {ss: req.session.acc});
+});
 
-router.get('/tangtraffic',function (req, res) {
-   res.render('addtraffic', {ss: req.session.acc});
+router.get('/mp3', function (req, res) {
+    var url = "https://mp3.zing.vn/xhr/media/get-source?type=audio&key=knxmtLHscSzJNkRymtbGLHtkWzxbsRaBa";
+
+    request(url, {
+        json: true
+    }, function (err, data) {
+        if (err) throw err;
+
+        // the JSON result
+        res.json({'url':'https:'+data.data.source['128']})
+    })
 });
 
 module.exports = router;
